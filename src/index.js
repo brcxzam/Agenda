414f600a3089435bee0ff996b16bb8e1510328d6
David Brc Zamorano <deyvith.lol@gmail.com>
2019-04-07 14:43:15 -0500

    Bulma and Sass

--- src/index.js
+++ src/index.js
@@ -4,12 +4,20 @@ const path = require('path');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./graphql/schema');
 const root = require('./graphql/root');
+const sassMiddleware = require('node-sass-middleware');
 const app = express();
 //Settings
 app.set('port', process.env.PORT || 3000);
 //Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public', 'css'),
+    indentedSyntax: false, // true = .sass and false = .scss
+    sourceMap: false,
+    prefix: '/css'
+}));
 //Routes
 app.use('/graphql', graphqlHTTP({
     schema: schema,
