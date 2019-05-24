08f2e893ffba79561555cdbcedad8204381c838c
Giselle <sahzade_mr@hotmail.com>
2019-05-24 09:21:21 -0500

    Paquetes

--- API/src/server.js
+++ API/src/server.js
@@ -1,22 +1,22 @@
-import express, { json, urlencoded } from 'express';
-import morgan from 'morgan';
-import api from './routes/api.routes';
-import cors from 'cors';
-const app = express();
+import express, { json, urlencoded } from 'express'
+import morgan from 'morgan'
+import api from './routes/api.routes'
+import cors from 'cors'
+const app = express()
 
 //Settings
-app.set('port', process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3001)
 
 //Middlewares
-app.use(cors());
-app.use(morgan('dev'));
-app.use(json());
-app.use(urlencoded({ extended: false }));
+app.use(cors())
+app.use(morgan('dev'))
+app.use(json())
+app.use(urlencoded({ extended: false }))
 
 //Routes
-app.use('/api', api);
+app.use('/api', api)
 
 //Static files
-app.use(express.static('public'));
+app.use(express.static('public'))
 
-export default app;
+export default app
