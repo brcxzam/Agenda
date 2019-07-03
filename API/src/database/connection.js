a57f714ea3307f47efdee274cb2782ee87d09818
David Brc Zamorano <deyvith.lol@gmail.com>
2019-07-03 09:22:55 -0500

    Connection

--- API/src/database/connection.js
+++ API/src/database/connection.js
@@ -1,6 +1,7 @@
 import Sequelize from 'sequelize'
 
-const sequelize = new Sequelize('mysql://root:@localhost:3306/agenda', {
+const sequelize = new Sequelize('agenda', 'root', '', {
+	dialect: 'mysql',
 	pool: {
 		max: 5,
 		min: 0,
